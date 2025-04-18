52d31022f5d63f93d730660a529262c888718255
sduddla <sduddla921@gmail.com>
2025-04-18 20:45:10 +0900

    App.tsx

--- react/btn-ui/src/App.tsx
+++ react/btn-ui/src/App.tsx
@@ -1,7 +1,17 @@
+import Button from './components/Button';
+
 export default function App() {
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen gap-10">
+      <Button type="button" className="bg-[#4f4f4f]">
+        Add
+      </Button>
+      <Button type="button" className="bg-[#ed4848]">
+        Cancel
+      </Button>
+      <Button type="button" className="bg-[#7D48ED]">
+        Success
+      </Button>
+    </div>
   );
 }
